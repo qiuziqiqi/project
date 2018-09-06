3867068c023384d7a15c02cb5af8fb478de6eaa1
simba <situowuyi@163.com>
2018-09-06 11:42:55 +0800

    no message

--- ziji/public/js/jquery.huax.js
+++ ziji/public/js/jquery.huax.js
@@ -1,19 +1,19 @@
-(function($){
-	$.extend({
-		"huax":function(){
-			$(".nav").css({
-				"list-style-type":"none",
-				"margin":"0",
-				"padding":"0",
-				"display":"none"
-			});
-			$(".nav").parent().hover(
-				function(){
-					$(this).find(".nav").slideDown();
-				},function(){
-					$(this).find(".nav").slideUp();
-				});
-			
-		}
-	});
+(function ($) {
+    $.extend({
+        "huax": function () {
+            $(".nav").css({
+                "list-style-type": "none",
+                "margin": "0",
+                "padding": "0",
+                "display": "none"
+            });
+            $(".nav").parent().hover(
+                function () {
+                    $(this).find(".nav").slideDown();
+                }, function () {
+                    $(this).find(".nav").slideUp();
+                });
+
+        }
+    });
 }(jQuery));
