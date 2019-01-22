de53722481f65f0e4ece2d090e3760659e0217f2
root <root@instance-2.c.hybirdauth.internal>
2019-01-22 18:16:50 +0800

    适应线上版本

--- ziji/admin.php
+++ ziji/admin.php
@@ -2,7 +2,7 @@
 header("content-type:text/html;charset=utf-8");
 
 define("BIND_MODULE","Admin");
-define("APP_PATH","application/");
+define("APP_PATH","Application/");
 define("APP_DEBUG",true);
 define("BUILD_DIR_SECURE",false);
 include_once 'library/ThinkPHP/ThinkPHP.php';
--- ziji/index.php
+++ ziji/index.php
@@ -1,6 +1,6 @@
 <?php
 	define("BIND_MODULE","Home");
-	define("APP_PATH","application/");
+	define("APP_PATH","Application/");
 	define("APP_DEBUG",true);
 	define("BUILD_DIR_SECURE",false);
 require 'library/ThinkPHP/ThinkPHP.php';
